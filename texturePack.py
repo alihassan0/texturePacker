db986bf922d23e7bfb621124a9e9ade80ee3236f
Ali Hassan <alithephantom@gmail.com>
2017-01-12 21:09:40 +0200

    add python json exporter

--- texturePack.py
+++ texturePack.py
@@ -1,21 +1,24 @@
 from os import listdir
 from os.path import isfile, join
-from util import crop,stamp
+from util import crop,stamp, Frame, dumper
 
 import cv2
 import numpy as np
+import json
 
 path = "assets"
-dict = {}
+jsonObject = {'frames':[],'meta':{}}
 
 #get fileNames
 fileNames = [f for f in listdir(path) if isfile(join(path, f))]
 
 #load all images in a dict
 for fileName in fileNames:
-    dict[fileName] = cv2.imread(path+'/'+fileName, cv2.IMREAD_UNCHANGED);
+    img = cv2.imread(path+'/'+fileName, cv2.IMREAD_UNCHANGED)
+    frame = Frame(fileName, img)
+    jsonObject['frames'].append(frame)
 
-#create Texture atlass images
+# #create Texture atlass images
 width = 512;
 height = 512;
 img = np.zeros((height, width,4), np.uint8)
@@ -23,16 +26,17 @@ img = np.zeros((height, width,4), np.uint8)
 lastX = 0
 lastY = 0
 maxHeightInRow = 0
-for key in dict:
+for frame in jsonObject['frames']:
 
     ## trim image
-    image_data_bw = dict[key].max(axis=2)
+    image_data_bw = frame.img.max(axis=2)
     non_empty_columns = np.where(image_data_bw.max(axis=0)>0)[0]
     non_empty_rows = np.where(image_data_bw.max(axis=1)>0)[0]
     cY1, cY2, cX1, cX2 = min(non_empty_rows), max(non_empty_rows), min(non_empty_columns), max(non_empty_columns)
-    dict[key] = crop(dict[key], cX1 , cY1, cX2+1 , cY2+1)
+    frame.spriteSourceSize =  {"x":cX1,"y":cY1,"w":cX2-cX1+1,"h":cY2-cY1+1}
+    frame.img = crop(frame.img, cX1 , cY1, cX2+1 , cY2+1)
     
-    h,w,c = dict[key].shape
+    h,w,c = frame.img.shape
     
     if lastX + w >= 512 :
         lastY += maxHeightInRow
@@ -42,11 +46,19 @@ for key in dict:
     if h > maxHeightInRow:
         maxHeightInRow = h 
 
-    stamp(dict[key],img,lastX,lastY)
+    stamp(frame.img,img,lastX,lastY)
+    frame.frame =  {"x":lastX,"y":lastY,"w":cX2-cX1+1,"h":cY2-cY1+1}
     lastX += w
+    
 
+# with open('output/output.json', 'w') as outfile:
+#     json.dumps(jsonObject, outfile, )
 
-cv2.imwrite("output/L5_output.png", img)
+import io, json
+with io.open('output/myTest.json', 'w', encoding='utf-8') as f:
+  f.write(unicode(json.dumps(jsonObject, default=dumper, indent=4, sort_keys=True, ensure_ascii=False)))
+
+cv2.imwrite("output/myTest.png", img)
 
 cv2.imshow("opencv",img)
 cv2.waitKey(0)
--- util.py
+++ util.py
@@ -1,7 +1,37 @@
-
+import json
 def crop(img, x, y, width, height):
     return img[y:height, x:width] # Crop from x, y, w, h -> 100, 200, 300, 400
 
 def stamp(src, dst, x, y):
     h,w,c = src.shape
     dst[y:y+h,x:x+w] = src
+
+class Frame:
+    def __init__(self, filename, img):
+        self.filename = filename
+        self.img = img
+        self.rotated = False
+        self.trimmed = True
+        self.index = 0
+        self.sourceSize = {"w":img.shape[1],"h":img.shape[0]}
+        self.pivot ={"x":0.5,"y":0.5}
+    ### end getP function
+
+    def toJSON(self):
+        d = {
+            "filename" :self.filename,
+            'rotated' :self.rotated,
+            'trimmed' :self.trimmed,
+            'index' :self.index,
+            'sourceSize' :self.sourceSize,
+            'pivot' :self.pivot,
+            'spriteSourceSize': self.spriteSourceSize,
+            'frame': self.frame
+        }
+        return d
+
+def dumper(obj):
+    try:
+        return obj.toJSON()
+    except:
+        return obj.__dict__
