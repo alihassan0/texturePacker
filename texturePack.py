9bbea7aa3a61ad27287b2e286a559956be64fc5e
Ali Hassan <alithephantom@gmail.com>
2017-01-12 12:14:56 +0200

    Add 2d packing

--- texturePack.py
+++ texturePack.py
@@ -21,16 +21,30 @@ height = 512;
 img = np.zeros((height, width,4), np.uint8)
 
 lastX = 0
+lastY = 0
+maxHeightInRow = 0
 for key in dict:
+
+    ## trim image
     image_data_bw = dict[key].max(axis=2)
     non_empty_columns = np.where(image_data_bw.max(axis=0)>0)[0]
     non_empty_rows = np.where(image_data_bw.max(axis=1)>0)[0]
     cY1, cY2, cX1, cX2 = min(non_empty_rows), max(non_empty_rows), min(non_empty_columns), max(non_empty_columns)
-    
     dict[key] = crop(dict[key], cX1 , cY1, cX2+1 , cY2+1)
+    
     h,w,c = dict[key].shape
-    stamp(dict[key],img,lastX,0)
+    
+    if lastX + w >= 512 :
+        lastY += maxHeightInRow
+        lastX = 0
+        maxHeightInRow = 0
+    
+    if h > maxHeightInRow:
+        maxHeightInRow = h 
+
+    stamp(dict[key],img,lastX,lastY)
     lastX += w
+
 
 cv2.imwrite("output/L5_output.png", img)
 
